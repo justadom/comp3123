68cd4de00c45471ce4ab73882d22dbc8c8d8798f
Dominic <frydrykdominic@gmail.com>
2025-10-20 23:47:50 -0400

    lab7 done

---
+++ labs/wk7/wk7/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        
+        <h1>          WELCOME TO FULLSTACK DEVELOPMENT - 1</h1>
+        <h2>          React JS Programming Week09 Lab exersize </h2>
+        <h3>          101361317</h3>
+        <h4>          Dominic Frydryk</h4>
+        <h5>          Gearogebrown college, Toronto </h5>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
